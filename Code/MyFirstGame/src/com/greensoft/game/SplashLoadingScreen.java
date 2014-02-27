898df3761aa172f02260ccce895d2c5811aabd01
Ramesh <ramesh.murugaswamy@gmail.com>
2014-02-27 14:20:32 +0530

    commit to working branch

--- Code/MyFirstGame/src/com/greensoft/game/SplashLoadingScreen.java
+++ Code/MyFirstGame/src/com/greensoft/game/SplashLoadingScreen.java
@@ -16,7 +16,7 @@ public class SplashLoadingScreen extends Screen {
     @Override
     public void update(float deltaTime) {
         Graphics g = game.getGraphics();
-        Assets.splash = g.newImage("splashtemp.jpg", ImageFormat.RGB565);
+        Assets.splash = g.newImage("splashworkingbranch.jpg", ImageFormat.RGB565);
         game.setScreen(new LoadingScreen(game));
     }
 
